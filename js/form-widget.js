862843a242a59b82a93c933e259859e09b1bd6ef
Damian Duda <dduda@nexway.com>
2013-08-01 15:26:40 +0200

    form widget: styles update

--- js/form-widget.js
+++ js/form-widget.js
@@ -5,47 +5,47 @@
 *
 */
 
-
-lang_click = function() {
-	$current_id = $(this).parent().parent().attr('id');
-	$('#' + $current_id + ' .select-language').children('option[value="select"]').attr('selected', false);
-	$('#' + $current_id + ' .select-language').children('option[value="' + $(this).attr('id') + '"]').attr('selected', true);
-	$('#' + $current_id + ' .current-language input.new-word').css('display', 'none');
-	$('#' + $current_id + ' .current-language input.translated').remove();
-	$input = $(this).children('input');
-	$str = '<input type="text" name="' + $input.attr('name') + '" value="' + $input.val() + '" class="m-wrap translated" />';
-	$($str).appendTo($('#' + $current_id + ' .current-language'));
-	$('#' + $current_id + '.form-translation').addClass('show');
-
-}
-
-new_click = function() {
-	$current_id = $(this).parent().attr('id');
-	$('#' + $current_id + ' .select-language').children('option[selected="selected"]').attr('selected', false);
-	$('#' + $current_id + ' .select-language').children('option[value="select"]').attr('selected', true);
-	$('#' + $current_id + ' .current-language input.translated').remove();
-	$('#' + $current_id + ' .current-language input.new-word').attr('value', '').attr('placeholder', 'Text to translate').css('display', 'inline-block');
-	$('#' + $current_id + '.form-translation').addClass('show');
-	$('#' + $current_id + ' #apply').click(function() {
-		$selected = $('#' + $current_id + ' .select-language option:selected').attr('value');
-//		if ($selected = ) {
-		$translation = $('#' + $current_id + ' .new-word').val();
-		$str = '<span id="' + $selected  + '" class="chosen-language">' + $selected;
-		$str += '<a href="#" class="remove icon-remove"></a>';
-		$str += '<input class="m-wrap" type="hidden" name="translation[' + $selected + ']" value="' + $translation + '"/>';
-		$str += '</span>';
-		$object = $($str).insertAfter($('#' + $current_id + ' .language-tabs').children().last());
-		$object.click(lang_click);
-		$object.children(".remove").click(remove_click);
-		return false; //link deactivated
-	});
-}
-
-remove_click = function() {
-	$(this).parent().remove();
-}
-
 jQuery(function() {
+	lang_click = function() {
+		$current_id = $(this).parent().parent().attr('id');
+		$('#' + $current_id + ' .select-language').children('option[value="select"]').attr('selected', false);
+		$('#' + $current_id + ' .select-language').children('option[value="' + $(this).attr('id') + '"]').attr('selected', true);
+		$('#' + $current_id + ' .current-language input.new-word').css('display', 'none');
+		$('#' + $current_id + ' .current-language input.translated').remove();
+		$input = $(this).children('input');
+		$str = '<input type="text" name="' + $input.attr('name') + '" value="' + $input.val() + '" class="m-wrap translated" />';
+		$($str).appendTo($('#' + $current_id + ' .current-language'));
+		$('#' + $current_id + '.form-translation').addClass('show');
+
+	}
+
+	new_click = function() {
+		$current_id = $(this).parent().attr('id');
+		$('#' + $current_id + ' .select-language').children('option[selected="selected"]').attr('selected', false);
+		$('#' + $current_id + ' .select-language').children('option[value="select"]').attr('selected', true);
+		$('#' + $current_id + ' .current-language input.translated').remove();
+		$('#' + $current_id + ' .current-language input.new-word').attr('value', '').attr('placeholder', 'Text to translate').css('display', 'inline-block');
+		$('#' + $current_id + '.form-translation').addClass('show');
+		$('#' + $current_id + ' #apply').click(function() {
+			$selected = $('#' + $current_id + ' .select-language option:selected').attr('value');
+	//		if ($selected = ) {
+			$translation = $('#' + $current_id + ' .new-word').val();
+			$str = '<span id="' + $selected  + '" class="chosen-language">' + $selected;
+			$str += '<a href="#" class="remove icon-remove"></a>';
+			$str += '<input class="m-wrap" type="hidden" name="translation[' + $selected + ']" value="' + $translation + '"/>';
+			$str += '</span>';
+			$object = $($chosen).insertAfter($('#' + $current_id + ' .language-tabs').children().last());
+			$object.click(lang_click);
+			$object.children(".remove").click(remove_click);
+			return false; //link deactivated
+		});
+	}
+
+	remove_click = function() {
+		$(this).parent().remove();
+	}
+
+
 	$('.form-translation').prepend('<span class="add-on open-translation"><i class="icon-reorder"></i><i class="icon-caret-up"></i></span>');
 	$str = '<div class="translation-options">\n';
 	$str += '<div class="translation-content"\n>';
@@ -89,6 +89,7 @@ jQuery(function() {
 
 	$('.open-translation').click(new_click);
 	$('.chosen-language').click(lang_click);
+
 	$('a.remove').click(remove_click);
 
   
