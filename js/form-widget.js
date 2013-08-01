83af9534a6a88d64de986d2f1f49ed1e58c95c54
Ariana Las <ariana.las@gmail.com>
2013-08-01 10:28:50 +0200

    Finish structure changes with textarea

--- js/form-widget.js
+++ js/form-widget.js
@@ -10,11 +10,12 @@ lang_click = function() {
 	$current_div = $(this).parent().parent();
 	$current_div.find('.select-language').children('option[value="select"]').attr('selected', false);
 	$current_div.find('.select-language').children('option[value="' + $(this).attr('id') + '"]').attr('selected', true);
-	$current_div.find('.current-language input.new-word').css('display', 'none');
-	$current_div.find('.current-language input.translated').remove();
+	$current_div.find('.current-language .new-word').css('display', 'none');
+	$current_div.find('.current-language .translated').remove();
 	$input = $(this).children('input');
-	$str = '<input type="text" name="' + $input.attr('name') + '" value="' + $input.val() + '" class="m-wrap translated" />';
+	$str = '<textarea name="' + $input.attr('name') + '" class="m-wrap translated" rows="1"></textarea>';
 	$current_div.find('.current-language').prepend($str);
+	$current_div.find('.current-language .translated').append($input.val());
 	$current_div.addClass('show');
 
 }
@@ -24,8 +25,8 @@ new_click = function() {
 
 	$current_div.find('.select-language').children('option[selected="selected"]').attr('selected', false);
 	$current_div.find('.select-language').children('option[value="select"]').attr('selected', true);
-	$current_div.find('.current-language input.translated').remove();
-	$current_div.find('.current-language input.new-word').attr('value', '').attr('placeholder', 'Text to translate').css('display', 'inline-block');
+	$current_div.find('.current-language .translated').remove();
+	$current_div.find('.current-language .new-word').attr('value', '').attr('placeholder', 'Text to translate').css('display', 'inline-block');
 	$current_div.addClass('show');
 }
 
@@ -42,8 +43,11 @@ apply_click = function() {
 		$str += '<input class="m-wrap" type="hidden" name="' + $name + '[' + $selected + ']" value="' + $translation + '"/>';
 		$str += '</span>';
 		$object = $($str).appendTo($current_div.find('.language-tabs'));
-		$object.css({backgroundColor: "#FBC21F"});
+		$object.css({backgroundColor: "#ffb848"});
 		$object.animate({backgroundColor: "#eee"}, 700);
+		$object.mouseover(function() {
+			$(this).css({backgroundColor: "#e1e1e1"});
+		});
 		$object.click(lang_click);
 		$object.children(".remove").click(remove_click);
 	}
@@ -63,39 +67,41 @@ remove_click = function() {
 
 jQuery(function() {
 	$('.lang-translation').wrap('<div class="input-prepend form-translation" />')
-	$('.form-translation').prepend('<span class="add-on open-translation"><i class="icon-reorder"></i><i class="icon-caret-up"></i></span>');
-	$str = '<div class="translation-options">\n';
-	$str += '<div class="translation-content"\n>';
-	$str += '<select class="select-language select2" tabindex="1">\n';
-	$str += '<option value="select" selected="selected">Select language</option>';
-	$str += '<option value="EN">English</option>';
-	$str += '<option value="DE">German</option>';
-	$str += '<option value="FR">French</option>';
-	$str += '<option value="ES">Spanish</option>';
-	$str += '<option value="PT">Portuguese</option>';
-	$str += '<option value="PL">Polish</option>';
-	$str += '<option value="JP">Japanese</option>';
-	$str += '</select>';
-	$str += '<div class="current-language">';
-	$str += '<input class="m-wrap new-word" type="text" placeholder="Text to translate" >';
-	$str += '<a href="/" class="btn blue apply">Apply</a>';
-	$str += '</div>';
-	$str += '<span class="hide-border"></span>';
-	$str += '</div></div>';
-	$($str).insertAfter('.form-translation input');
-	$str = '<div class="language-tabs">';
-	/* SOME EXAMPLES OF TRANSLATIONS
-	$str += '<span id="FR" class="chosen-language">FR';
-	$str += '<a href="/" class="remove icon-remove"></a>';
-	$str += '<input class="m-wrap" type="hidden" name="translation1[FR]" value="po-francusku"/>';
-	$str += '</span>';
-	$str += '<span id="EN" class="chosen-language">EN';
-	$str += '<a href="/" class="remove icon-remove"></a>';
-	$str += '<input class="m-wrap" type="hidden" name="translation1[EN]" value="po-angielsku"/>';
-	$str += '</span>';
-	*/
-	$str += '</div>';
-	$($str).insertAfter('.translation-options');
+	$('.form-translation').each(function() {
+		$(this).prepend('<span class="add-on open-translation"><i class="icon-reorder"></i><i class="icon-caret-up"></i></span>');
+		$str = '<div class="translation-options">\n';
+		$str += '<div class="translation-content"\n>';
+		$str += '<select class="select-language select2" tabindex="1">\n';
+		$str += '<option value="select" selected="selected">Select language</option>';
+		$str += '<option value="EN">English</option>';
+		$str += '<option value="DE">German</option>';
+		$str += '<option value="FR">French</option>';
+		$str += '<option value="ES">Spanish</option>';
+		$str += '<option value="PT">Portuguese</option>';
+		$str += '<option value="PL">Polish</option>';
+		$str += '<option value="JP">Japanese</option>';
+		$str += '</select>';
+		$str += '<div class="current-language">';
+		$str += '<textarea class="m-wrap new-word" placeholder="Text to translate" rows="1"></textarea>';
+		$str += '<a href="/" class="btn blue apply">Apply</a>';
+		$str += '</div>';
+		$str += '<span class="hide-border"></span>';
+		$str += '</div></div>';
+		$($str).appendTo($(this));
+		$str = '<div class="language-tabs">';
+		/* SOME EXAMPLES OF TRANSLATIONS
+		$str += '<span id="FR" class="chosen-language">FR';
+		$str += '<a href="/" class="remove icon-remove"></a>';
+		$str += '<input class="m-wrap" type="hidden" name="translation1[FR]" value="po-francusku"/>';
+		$str += '</span>';
+		$str += '<span id="EN" class="chosen-language">EN';
+		$str += '<a href="/" class="remove icon-remove"></a>';
+		$str += '<input class="m-wrap" type="hidden" name="translation1[EN]" value="po-angielsku"/>';
+		$str += '</span>';
+		*/
+		$str += '</div>';
+		$($str).insertAfter($(this).children('.translation-options'));
+	});
 
 
 
